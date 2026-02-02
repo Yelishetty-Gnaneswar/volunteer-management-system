7a0558ead4d3f592a4a8bd582c1ed98ea0a31035
Yelishetty-Gnaneswar <yelishettygnaneswar@gmail.com>
2026-02-02 08:30:45 +0530

    Use Render backend URL via env

--- volunteerhub-frontend/src/api/axios.js
+++ volunteerhub-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.https://volunteer-management-system-isp4.onrender.com,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
