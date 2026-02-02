8035b03859fb5b9f6386b35fc976a4086ad5f0d4
Yelishetty-Gnaneswar <yelishettygnaneswar@gmail.com>
2026-02-02 08:22:46 +0530

    Allow Vercel frontend in CORS

--- src/main/java/com/infosys/volunteer/management/config/CorsConfig.java
+++ src/main/java/com/infosys/volunteer/management/config/CorsConfig.java
@@ -3,6 +3,7 @@ package com.infosys.volunteer.management.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
@@ -11,11 +12,11 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:5173",
-                        "https://volunteer-management-system-107o5dslk.vercel.app"
+                        "https://volunteer-management-system-isp4.onrender.com",
+                        "https://volunteer-management-system-gnugogy3z.vercel.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
-
