883fd645ef1877784c02ac887b5907c34652c99a
Yelishetty-Gnaneswar <yelishettygnaneswar@gmail.com>
2026-02-02 08:38:20 +0530

    axios update

--- volunteerhub-frontend/src/api/axios.js
+++ volunteerhub-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.https://volunteer-management-system-isp4.onrender.com,
   headers: {
     "Content-Type": "application/json",
   },
